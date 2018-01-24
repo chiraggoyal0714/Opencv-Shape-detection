b526febef82d0e8d0a6e527897649ed909e07538
chiraggoyal0714 <chirag.goyal0714@gmail.com>
2018-01-24 20:05:42 +0530

    Proper documentation added

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Opencv-Shape-detection
+
+
+
+
+#### What is Opencv-Shape-detection
+**Opencv-Shape-detection** is a script which tells about the Centroid, color and the shape of the object in the given image
+
+We've written a short manual explaining how you can adapt it for your own needs.
+
+* How to setup?
+***
+
+## Getting started
+
+### Install dependecies
+[Install python](https://www.python.org/downloads/) in your system
+[Install opencv2](https://gist.github.com/arthurbeggs/06df46af94af7f261513934e56103b30)
+[Install numpy](https://docs.scipy.org/doc/numpy-1.10.1/user/install.html)
+
+### Execute
+
+### Linux or MacOS
+open terminal and navigate to the folder and type the following command
+'''html
+ python detect_color_shape.py shapesPhoto.png
+
+'''
+where shapesPhoto.png sample Image
+
+### Windows
+open Idle and simple run the **detect_color_shape.py** file
+
+
+** Created by Chirag Goyal **
--- detect_color_shape.py
+++ detect_color_shape.py
@@ -1,7 +1,4 @@
-# USAGE
-# python detect_color.py --image example_shapes.png
 
-# import the necessary packages
 from pyimagesearch.shapedetector import ShapeDetector
 from pyimagesearch.colorlabeler import ColorLabeler
 import argparse
@@ -10,42 +7,34 @@ import cv2
 import numpy as np
 
 
-
-# load the image and resize it to a smaller factor so that
-# the shapes can be approximated better
 image = cv2.imread("shapesPhoto.png")
 l,h,c=image.shape
-print l,h,c
+#print l,h,c
 r = 150.0 / image.shape[1]
 dim = (150, int(image.shape[0] * r))
 resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
 lower_white = np.array([220, 220, 220], dtype=np.uint8)
 upper_white = np.array([255, 255, 255], dtype=np.uint8)
-mask = cv2.inRange(resized, lower_white, upper_white) # could also use threshold
+mask = cv2.inRange(resized, lower_white, upper_white) 
 res = cv2.bitwise_not(resized, resized, mask)
-cv2.imshow('res', res) # gives black backg
+#cv2.imshow('res', res)
 resized = imutils.resize(res, width=898)
 ratio = image.shape[0] / float(resized.shape[0])
 
-
-# blur the resized image slightly, then convert it to both
-# grayscale and the L*a*b* color spaces
 blurred = cv2.GaussianBlur(resized, (5, 5), 0)
 gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
 lab = cv2.cvtColor(blurred, cv2.COLOR_BGR2LAB)
 thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)[1]
-cv2.imshow("Thresh", thresh)
+#cv2.imshow("Thresh", thresh)
 
-# find contours in the thresholded image
 cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 
-# initialize the shape detector and color labeler
+
 sd = ShapeDetector()
 cl = ColorLabeler()
 
-# loop over the contours
 for c in cnts:
 	# compute the center of the contour
 	M = cv2.moments(c)
@@ -56,9 +45,6 @@ for c in cnts:
 	shape = sd.detect(c)
 	color = cl.label(lab, c)
 
-	# multiply the contour (x, y)-coordinates by the resize ratio,
-	# then draw the contours and the name of the shape and labeled
-	# color on the image
 	c = c.astype("float")
 	c *= ratio
 	c = c.astype("int")
@@ -67,6 +53,5 @@ for c in cnts:
 	cv2.putText(image, text, (cX, cY),
 		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 2)
 
-	# show the output image
 	cv2.imshow("Image", image)
 	cv2.waitKey(0)
